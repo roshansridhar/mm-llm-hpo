b24dc72eb4a169e42281eedb9fe7b2500f11a83b
Roshan Sridhar <roshan.sridhar@oracle.com>
2024-04-21 11:35:22 -0700

    llm-hpo with gpt-4-vision

--- optimizers/vision_optimizer.py
+++ optimizers/vision_optimizer.py
@@ -19,13 +19,14 @@ class GPT4VisionOptimizer(BaseOptimizer):
             plot_path = self.plot_validation_loss()
             plot_base64 = self.encode_image(plot_path) if plot_path else None
             prompt = self.generate_prompt(self.history)
+            prompt = prompt.strip("Config:") + " The graph of validation loss versus hyperparameter values over previous trials is attached for your reference. Config:"
             config = None
             while config is None:
                 headers = {"Content-Type": "application/json", "Authorization": f"Bearer {self.api_key}"}
                 payload = {"model": "gpt-4-turbo",
                            "messages": [{"role": "user", "content": [{"type": "text", "text": prompt}]}],
-                           "temperature": 0.1,
-                           "max_tokens": 500,
+                           "temperature": 0,
+                           "max_tokens": 2000,
                            "top_p": 1}
                 if plot_base64:
                     payload['messages'][0]['content'].append(
@@ -47,7 +48,11 @@ class GPT4VisionOptimizer(BaseOptimizer):
                     except:
                         logger.debug(f"response is not json format. Request again. Response: {response['choices'][0]['message']['content']}")
                         continue  # If decoding fails, retry the request
-
+            print(prompt)
+            print(response)
             score = self.benchmarker.evaluate(config)
             self.history.append((iteration, config, score))
+            # self.history.append((iteration, {'C': 0.0009765625, 'gamma': 0.0009765625},
+            #                      {'train_loss': 1.0, 'validation_loss': 0.29276500280426243,
+            #                       'cost': 0.3861689567565918}))
         return self.history
